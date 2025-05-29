7ec11b82bca3800faaa152e7da4bc5393892c3c0
RuslanRuslan89 <ruslankon89@gmail.com>
2025-05-29 13:28:07 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
